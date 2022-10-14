e359c11e034292efe4f8d9d7e7ed579262a553b8
Suddha M <suddha@banxa.com>
2022-10-14 12:48:23 +0300

    Add navlink with active class

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,14 @@
+import { NavLink } from "solid-app-router";
 import { Component } from "solid-js";
 
 const Nav: Component = () => {
+    const primaryLinkClass = "text-success";
+    
     return (
-        <div>
-            <h2>Nav</h2>
-        </div>
+        <nav class="my-4">
+            <NavLink href="/" class="btn btn-link text-secondary" end activeClass={primaryLinkClass}>Home</NavLink>
+            <NavLink href="/saved-repos" class="btn btn-link text-secondary" activeClass={primaryLinkClass}>Saved repos</NavLink>
+        </nav>
     );
 };
 
